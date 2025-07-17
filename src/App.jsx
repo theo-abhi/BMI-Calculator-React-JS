76c1f7a84d6fd17381b760db7bebd3b7674c7dab
theo-abhi <theo.abhi2793@gmail.com>
2025-07-17 17:38:35 +0530

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import BMICalculator from "./components/BMI-Calculator/BMICalculator";
+// import BMICalculator from "./components/BMI-Calculator/BMICalculator2";
+
+function App() {
+  return (
+    <>
+      <BMICalculator />
+    </>
+  );
+}
+
+export default App;
